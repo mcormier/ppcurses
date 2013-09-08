8edf248374663da279a6f18a57c6e61cf86970e2
Matthieu Cormier <mcormier@cs.dal.ca>
2013-09-08 14:56:18 -0300

    Style

--- lib/ppcurses/actions/GetEnumeratedStringAction.rb
+++ lib/ppcurses/actions/GetEnumeratedStringAction.rb
@@ -1,5 +1,6 @@
 require_relative 'BaseAction.rb'
 
+#noinspection RubyResolve
 module PPCurses
 
   class GetEnumeratedStringAction < PromptAction
@@ -10,43 +11,44 @@ module PPCurses
       super(prompt)
 
       # verify enumeration is an array
-      if ( enumeration.respond_to?('each_with_index') == false ) then
+      unless enumeration.respond_to?('each_with_index') then
         raise
       end
+
       @options = enumeration
-      @currOption = 0
+      @current_option = 0
     end
 
-    def printPrompt
+    def print_prompt
       super()
       @options.each_with_index  do |option, index|
         @win.addstr(option)
-        if ( index == @currOption ) then 
-          @win.addstr(" [X] ")
+        if index == @current_option then
+          @win.addstr(' [X] ')
         else
-          @win.addstr(" [ ] ")
+          @win.addstr(' [ ] ')
         end
 
       end
     end
 
     def execute
-      printPrompt()
+      print_prompt()
       # Enables reading arrow keys in getch 
       @win.keypad(true)
-      while(1)
+      while 1
         noecho
         c = @win.getch
 
-        if c == KEY_LEFT then @currOption = @currOption-1 end
-        if c == KEY_RIGHT then @currOption= @currOption+1 end
+        if c == KEY_LEFT then @current_option = @current_option-1 end
+        if c == KEY_RIGHT then @current_option= @current_option+1 end
         if c == 10 then break end
 
-        if (@currOption < 0 ) then @currOption = @options.length-1 end
-        if (@currOption > @options.length-1 ) then @currOption = 0 end
+        if @current_option < 0 then @current_option = @options.length-1 end
+        if @current_option > @options.length-1 then @current_option = 0 end
 
         echo
-        printPrompt()
+        print_prompt()
       end
       echo
       # Go to next line so that further actions to overwrite
@@ -55,7 +57,7 @@ module PPCurses
     end
 
     def data
-      @options[@currOption]
+      @options[@current_option]
     end 
 
   end
