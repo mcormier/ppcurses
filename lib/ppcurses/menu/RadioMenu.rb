77effa652346db337f74b35481e5860cc88ffe7c
Matthieu Cormier <mcormier@cs.dal.ca>
2013-09-08 14:10:01 -0300

    style

--- lib/ppcurses/menu/RadioMenu.rb
+++ lib/ppcurses/menu/RadioMenu.rb
@@ -4,36 +4,37 @@ require_relative 'BaseMenu.rb'
 module PPCurses
   class RadioMenu  < BaseMenu
 
-    def initialize( menuItems, actionItems )
+    def initialize( menu_items, action_items )
       @items = Array.new
+      @actions = Array.new
 
-      @menuLength = 0
+      @menu_length = 0
 
-       menuItems.each do |item|
+       menu_items.each do |item|
          @items.push item
-          @menuLength += item.length + 5
+          @menu_length += item.length + 5
        end
 
       @selection = 0
 
-      unless actionItems.nil? 
-        @actions = Array.new
-        actionItems.each do |item|
+      unless action_items.nil?
+
+        action_items.each do |item|
           @actions.push item      
         end
       end 
      
-      winWidth = @menuLength + 4
+      w_width = @menu_length + 4
 
-      @win = Window.new(3, winWidth ,0, (cols - winWidth) / 2)
+      @win = Window.new(3, w_width ,0, (cols - w_width) / 2)
 
       @win.timeout=-1
       # Enables reading arrow keys in getch 
       @win.keypad(true)
     end
 
-    def show()
-      @win.box("|", "-")
+    def show
+      @win.box('|', '-')
       y = 1
       x = 2
 
@@ -41,7 +42,7 @@ module PPCurses
 
       for i in 0...@items.length
         @win.addstr( @items[i] )
-        if @selection == i then @win.addstr(" [*] ") else @win.addstr(" [ ] " ) end
+        if @selection == i then @win.addstr(' [*] ') else @win.addstr(' [ ] ') end
       end
 
      @win.refresh
@@ -49,14 +50,14 @@ module PPCurses
     end
 
 
-    def getMenuSelection()
+    def menu_selection
 
-      while(1)
+      while 1
         c = @win.getch
 
-        processed = self.handleMenuSelection(c)
+        self.handle_menu_selection(c)
 
-        if c == 27 then # ESCAPE
+        if c == 27 # ESCAPE
           @win.clear
           @win.refresh
           break
@@ -67,15 +68,15 @@ module PPCurses
     end
 
 
-    def handleMenuSelection(c)
+    def handle_menu_selection(c)
       n_choices = @items.length
 
-      if c == KEY_RIGHT then
+      if c == KEY_RIGHT
         if @selection < n_choices - 1 then @selection += 1 else @selection = 0 end
         self.show()
       end
 
-      if c == KEY_LEFT then
+      if c == KEY_LEFT
         if @selection > 0 then @selection -= 1 else @selection = n_choices-1 end
         self.show()
       end
@@ -90,7 +91,7 @@ module PPCurses
    
 
 
-    def close()
+    def close
       @win.close()
     end
 
