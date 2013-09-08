559670261ff669bda3f5ddf39004262a1236c843
Matthieu Cormier <mcormier@cs.dal.ca>
2013-09-08 16:12:18 -0300

    Style

--- test/getEnumStringAction.rb
+++ test/getEnumStringAction.rb
@@ -3,15 +3,14 @@
 require 'rubygems'
 require_relative '../lib/ppcurses.rb'
 
-action = PPCurses::GetEnumeratedStringAction.new("Media Type? ",
-              ["CD", "Vinyl", "MP3"] );
+action = PPCurses::GetEnumeratedStringAction.new('Media Type? ', %w(CD Vinyl MP3))
 
-def doAction(action)
+def do_action(action)
   action.show()
   action.execute()
 end
 
 screen = PPCurses::Screen.new()
-screen.run { doAction(action) }
+screen.run { do_action(action) }
 
-puts "Value input was: " + action.data()  
+puts 'Value input was: ' + action.data()
