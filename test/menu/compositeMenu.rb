9015f34ef634b3516151cd874d38a4c061e8557c
Matthieu Cormier <mcormier@cs.dal.ca>
2014-02-02 16:17:24 -0400

    Style

--- test/menu/compositeMenu.rb
+++ test/menu/compositeMenu.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-require_relative '../lib/ppcurses.rb'
+require_relative '../../lib/ppcurses.rb'
 
 def display_menu
   center_menu = PPCurses::Menu.new( [ 'Press', '<ESCAPE>', 'to Quit'], nil )
@@ -10,11 +10,11 @@ def display_menu
   main_menu = PPCurses::CompositeMenu.new(center_menu, radio_menu)
 
 
-  main_menu.show()
-  main_menu.menu_selection()
-  main_menu.close()
+  main_menu.show
+  main_menu.menu_selection
+  main_menu.close
 end
 
-screen = PPCurses::Screen.new()
-screen.run { display_menu() }
+screen = PPCurses::Screen.new
+screen.run { display_menu }
 
