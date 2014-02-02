b5e1ede38470fd601a399af826ded30a61757b5a
Matthieu Cormier <mcormier@cs.dal.ca>
2014-02-02 10:37:52 -0400

    Style

--- test/getBooleanAction.rb
+++ test/getBooleanAction.rb
@@ -6,11 +6,11 @@ require_relative '../lib/ppcurses.rb'
 action = PPCurses::GetBooleanAction.new('Is the sky blue? ')
 
 def do_boolean_action(action)
-  action.show()
-  action.execute()
+  action.show
+  action.execute
 end
 
-screen = PPCurses::Screen.new()
+screen = PPCurses::Screen.new
 screen.run { do_boolean_action(action) }
 
-puts 'Value input was: ' + action.data()
+puts 'Value input was: ' + action.data
