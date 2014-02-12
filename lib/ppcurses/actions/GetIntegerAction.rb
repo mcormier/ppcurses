a7c5973f9b26219b8164cccb2c17b88a86cfdea1
Matthieu Cormier <mcormier@cs.dal.ca>
2014-02-11 20:23:46 -0400

    Style

--- lib/ppcurses/actions/GetIntegerAction.rb
+++ lib/ppcurses/actions/GetIntegerAction.rb
@@ -6,17 +6,17 @@ module PPCurses
   class GetIntegerAction < PromptAction
 
     def execute
-     y = @win.cury()
+     y = @win.cury
      @data = ''
-     begin 
-       @win.setpos(y,x_padding())
-       @win.clrtoeol()
+     begin
+       @win.setpos(y, x_padding)
+       @win.clrtoeol
        @win.box('|', '-')
        @win.addstr(@prompt)
        echo
-       @data = @win.getstr()
+       @data = @win.getstr
        noecho
-     end while not @data =~ /^\d+$/ 
+     end until @data =~ /^\d+$/
     end
 
   end
