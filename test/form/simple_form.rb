8a5d9ee172ac025db4759ff98a029c5bcf0e1b39
Matthieu Cormier <mcormier@cs.dal.ca>
2014-10-27 20:22:51 -0300

    Add element to form and call show.

--- test/form/simple_form.rb
+++ test/form/simple_form.rb
@@ -8,9 +8,11 @@ require_relative '../../lib/ppcurses.rb'
 
 def display_form
 
-
-  first_name = PPCurses::InputElement.new("FirstName", 10)
+  first_name = PPCurses::InputElement.new('First Name', 10)
   form = PPCurses::Form.new(Curses)
+
+  form.elements.push(first_name)
+  form.show
 
   Curses.getch
 
