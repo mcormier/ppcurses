b00c700e0f6c672b6607aa50527c352eef887c6d
Matthieu Cormier <mcormier@cs.dal.ca>
2014-10-28 17:06:14 -0300

    Add handle_input

--- lib/ppcurses/form/form.rb
+++ lib/ppcurses/form/form.rb
@@ -3,6 +3,7 @@ module PPCurses
   class Form
 
     attr_accessor :elements
+    attr_accessor :selected_element
 
     def initialize (screen)
       @win = screen
@@ -10,6 +11,8 @@ module PPCurses
     end
 
     def show
+
+      @selected_element = @elements[0]
 
       y = 1
       x = 1
@@ -20,7 +23,39 @@ module PPCurses
         y = y + 1
       end
 
+      # TODO display a button to exit SUBMIT/CANCEL
+
     end
+
+
+    def handle_input
+
+      while 1
+        c = @win.getch
+
+        #not_processed = !self.handle_menu_selection(c)
+
+        if c == ESCAPE
+          # exit for now
+          break
+        end
+
+         if c == KEY_UP
+           # TODO -- give focus to previous element
+           break
+         end
+
+         if c == KEY_DOWN
+           # TODO -- give focus to next element
+           break
+         end
+
+       # @sub_menu.handle_menu_selection(c) if not_processed && @sub_menu
+
+      end
+
+    end
+
 
   end
 
