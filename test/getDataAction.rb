42749d711952ae8f458cb27a55eecd170529a313
Matthieu Cormier <mcormier@cs.dal.ca>
2014-11-01 14:12:07 -0300

    Style.

--- test/getDataAction.rb
+++ test/getDataAction.rb
@@ -3,17 +3,17 @@
 require 'rubygems'
 require_relative '../lib/ppcurses.rb'
 
-integer_action = PPCurses::GetIntegerAction.new("Input Integer : ")
-string_action = PPCurses::GetStringAction.new("Input your name: ")
+integer_action = PPCurses::GetIntegerAction.new('Input Integer : ')
+string_action = PPCurses::GetStringAction.new('Input your name: ')
 
 action = PPCurses::GetDataAction.new( [integer_action, string_action] )
 
 def do_integer_action(action)
-  action.show()
-  action.execute()
+  action.show
+  action.execute
 end
 
-screen = PPCurses::Screen.new()
+screen = PPCurses::Screen.new
 screen.run { do_integer_action(action) }
 
-puts 'Value input was: ' + action.data().to_s()
+puts 'Value input was: ' + action.data.to_s
