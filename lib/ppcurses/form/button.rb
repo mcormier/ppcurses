6faa42f2a9edb558afb2efafc710ce570979a412
Matthieu Cormier <mcormier@cs.dal.ca>
2014-11-02 16:05:45 -0400

    First drawing implementation

--- lib/ppcurses/form/button.rb
+++ lib/ppcurses/form/button.rb
@@ -1,6 +1,33 @@
 module PPCurses
 
   class Button
+
+    attr_accessor :label
+    attr_accessor :selected
+
+    def initialize(label)
+      @label = label
+    end
+
+    def show(screen)
+
+      x = screen.curx
+      y = screen.cury
+
+      screen.addstr(''.ljust(@label.length + 4, '-'))
+      y += 1
+      screen.setpos(y,x)
+      screen.attron(A_REVERSE) if @selected
+      screen.addstr("| #{@label} |")
+      screen.attroff(A_REVERSE) if @selected
+
+      y += 1
+      screen.setpos(y,x)
+      screen.addstr(''.ljust(@label.length + 4, '-'))
+
+    end
+
+
   end
 
 end
