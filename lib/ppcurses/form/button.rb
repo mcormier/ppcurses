4a0a90e4257d695b8337d823552f2dd82909a34d
Matthieu Cormier <mcormier@cs.dal.ca>
2014-11-02 16:12:46 -0400

    Add a ButtonPair class

--- lib/ppcurses/form/button.rb
+++ lib/ppcurses/form/button.rb
@@ -27,7 +27,44 @@ module PPCurses
 
     end
 
+    def width
+      @label.length + 4
+    end
 
   end
+
+  # For grouping two buttons together, i.e. SUBMIT/CANCEL
+  class ButtonPair
+
+
+
+    def initialize(button1_label, button2_label)
+      @button1 = Button.new(button1_label)
+      @button2 = Button.new(button2_label)
+    end
+
+    def show(screen)
+
+      # TODO -- create a Point class
+      x = screen.curx
+      y = screen.cury
+
+      @button1.show(screen)
+
+      x += @button1.width + 2
+      screen.setpos(y,x)
+
+      @button2.show(screen)
+
+    end
+
+    def handle_keypress( key )
+
+    end
+
+
+  end
+
+
 
 end
