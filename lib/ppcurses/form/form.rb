f7a9a6472b5d00da149779dd7a937fc2ccfeb267
Matthieu Cormier <mcormier@cs.dal.ca>
2014-11-02 20:45:43 -0400

    Use button submit instead of escape key

--- lib/ppcurses/form/form.rb
+++ lib/ppcurses/form/form.rb
@@ -50,27 +50,25 @@ module PPCurses
       while 1
         c = @win.getch
 
-        if c == ESCAPE
-          # exit for now
-          break
+        if c == KEY_UP or c == KEY_DOWN
+
+          selected_index = @elements.index(@selected_element)
+
+          if c == KEY_DOWN
+            (selected_index == n_choices-1) ? next_selection = 0 : next_selection = selected_index + 1
+          else
+            (selected_index == 0) ? next_selection = n_choices - 1 : next_selection =  selected_index - 1
+          end
+
+          set_selected_element(@elements[next_selection])
+        else
+          should_exit = @selected_element.handle_keypress(c)
+          if should_exit
+            break
+          end
         end
 
-         if c == KEY_UP or c == KEY_DOWN
-
-           selected_index = @elements.index(@selected_element)
-
-           if c == KEY_DOWN
-             (selected_index == n_choices-1) ? next_selection = 0 : next_selection = selected_index + 1
-           else
-             (selected_index == 0) ? next_selection = n_choices - 1 : next_selection =  selected_index - 1
-           end
-
-           set_selected_element(@elements[next_selection])
-         else
-           @selected_element.handle_keypress(c)
-         end
-
-         show
+      show
 
       end
 
