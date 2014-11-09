407105343d7f2d22f13164fda8fbd6e88d11974c
Matthieu Cormier <mcormier@cs.dal.ca>
2014-11-09 10:21:53 -0400

    Implement protocol methods.

--- lib/ppcurses/form/combo_box.rb
+++ lib/ppcurses/form/combo_box.rb
@@ -2,12 +2,26 @@ module PPCurses
 
   class ComboBox
 
+    attr_accessor :selected
+
     def initialize( label, options)
 
     end
 
     def show(screen)
 
+    end
+
+    def height
+      1
+    end
+
+    def set_curs_pos(screen)
+      curs_set(INVISIBLE)
+    end
+
+    def handle_keypress(key)
+      false
     end
 
   end
