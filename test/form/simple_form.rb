cdeac67cabcfaf12ff391fc4d0cef318b96030b5
Matthieu Cormier <mcormier@cs.dal.ca>
2014-11-10 19:49:53 -0400

    Adds an age input element.

--- test/form/simple_form.rb
+++ test/form/simple_form.rb
@@ -11,16 +11,18 @@ def display_form
 
   first_name = PPCurses::InputElement.new('First Name', 10)
   last_name = PPCurses::InputElement.new(' Last Name', 10)
+  age = PPCurses::InputElement.new_integer_only('      Age', 5)
 
-  gender = PPCurses::RadioButtonGroup.new('      Sex', ['Male', 'Female'])
+  gender = PPCurses::RadioButtonGroup.new('      Sex', %w(Male Female))
 
   form.add(first_name)
   form.add(last_name)
+  form.add(age)
   form.add(gender)
 
   form.handle_input
 
-  { first_name: first_name.value, last_name: last_name.value,
+  { first_name: first_name.value, last_name: last_name.value, age: age.value,
     gender: gender.current_option, form_submitted: form.submitted?}
 end
 
