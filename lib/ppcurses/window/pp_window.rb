f633c753b13b203c8be564a6b1a23013faa87011
Matthieu Cormier <mcormier@cs.dal.ca>
2014-11-18 18:53:22 -0400

    Add hack method to Window

--- lib/ppcurses/window/pp_window.rb
+++ lib/ppcurses/window/pp_window.rb
@@ -11,6 +11,29 @@ module PPCurses
       box('|', '-')
     end
 
+
+
+    # EXPERIMENTAL/HACK
+    #
+    # The following could be used to wrap all getch calls
+    # and support window resizes when the getch is blocking
+    # all threads.
+    #
+    def get_ch_handle_signals
+      got_input = false
+      until got_input
+        begin
+          c = getch
+          got_input = true
+        rescue NoMethodError
+          # Assuming a SIGWINCH occurred -- reposition..
+          c = ''
+        end
+      end
+
+      c
+    end
+
   end
 
 
