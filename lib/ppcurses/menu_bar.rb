dfeec3ff189ce9cfb54fb2a428deaa6eb39fa818
Matthieu Cormier <mcormier@cs.dal.ca>
2014-11-22 07:19:00 -0400

    menuItem stub class

--- lib/ppcurses/menu_bar.rb
+++ lib/ppcurses/menu_bar.rb
@@ -1,10 +1,29 @@
 module PPCurses
 
+
+
   class MenuBar
 
     # Expects screen to be PPCurses::Screen object
     def show(screen)
       screen.print_with_attribute( ' ' * screen.width, A_REVERSE )
+    end
+
+
+  end
+
+  # MenuBarItems ...
+  #
+  # q:Quit d:Del u:Undel
+  #
+  class MenuBarItem
+
+    attr_accessor :key
+    attr_accessor :label
+
+    def intiialize( key, label )
+      @key = key
+      @label = label
     end
 
 
