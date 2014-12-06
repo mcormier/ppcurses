9bbf877a0e4bccfae9114cb3a75172e6b21c010a
Matthieu Cormier <mcormier@cs.dal.ca>
2014-12-06 11:17:05 -0400

    Initial import

--- test/getIntegerAction.rb
+++ test/getIntegerAction.rb
@@ -6,11 +6,11 @@ require_relative '../lib/ppcurses.rb'
 action = PPCurses::GetIntegerAction.new('Input Integer : ')
 
 def do_integer_action(action)
-  action.show()
-  action.execute()
+  action.show
+  action.execute
 end
 
-screen = PPCurses::Screen.new()
+screen = PPCurses::Screen.new
 screen.run { do_integer_action(action) }
 
-puts 'Value input was: ' + action.data()
+puts "Value input was: #{action.data}"
