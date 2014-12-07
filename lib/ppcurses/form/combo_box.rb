f58789a89872b5a49e956a62795901fe706fe129
Matthieu Cormier <mcormier@cs.dal.ca>
2014-12-07 18:28:50 -0400

    Display users choice.

--- lib/ppcurses/form/combo_box.rb
+++ lib/ppcurses/form/combo_box.rb
@@ -6,11 +6,23 @@ module PPCurses
 
     DOWN_ARROW = '∇'
 
+    # Does the element have focus in the form?
     attr_accessor :selected
+
 
     def initialize(label, options)
       @label = label
       @options = options
+
+      @display_width = 13
+      @choice_made = false
+
+      @options.each do |option|
+        if option.length > @display_width
+          @display_width = option.length
+        end
+      end
+
     end
 
     def show(screen)
@@ -20,7 +32,8 @@ module PPCurses
       @combo_display_point = screen.cur_point
 
       screen.attroff(A_REVERSE) if @selected
-      screen.addstr(" - select - #{DOWN_ARROW}")
+
+      screen.addstr(display_string)
     end
 
     def height
@@ -34,18 +47,36 @@ module PPCurses
     def handle_keypress(key)
 
       if key == ENTER
-        # TODO - Sub menu must handle ENTER
-        options_menu = PPCurses::Menu.new( @options, nil )
 
-        options_menu.set_origin(@combo_display_point)
+        if  @options_menu.nil?
+          @options_menu = PPCurses::ChoiceMenu.new( @options )
+        end
 
-        options_menu.show
-        options_menu.menu_selection
-        options_menu.close
+        @options_menu.set_origin(@combo_display_point)
+
+        @options_menu.show
+        @options_menu.menu_selection
+
+        if @options_menu.pressed_enter
+          @choice_made = true
+        end
+
       end
 
       false
     end
+
+    #------------------------------------------------
+    protected
+    def display_string
+      if @choice_made
+        return ' '+ "#{@options[@options_menu.selection].center(@display_width,'-')} #{DOWN_ARROW}"
+      end
+
+      ' ' + ' select '.center(@display_width,'-') + " #{DOWN_ARROW}"
+    end
+
+
 
   end
 
