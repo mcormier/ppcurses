38a9e4107779869fd35d1dd467df3ef3222af706
Matthieu Cormier <mcormier@cs.dal.ca>
2014-12-18 06:11:05 -0400

    Initial import.  Stub class

---
+++ lib/ppcurses/view.rb
@@ -0,0 +1,39 @@
+module PPCurses
+
+  # View Hierarchy information:
+  #
+  # https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/CocoaViewsGuide/WorkingWithAViewHierarchy/WorkingWithAViewHierarchy.html
+
+  # Based loosely on ...
+  #
+  # https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSView_Class/index.html#//apple_ref/occ/instp/NSView/subviews
+
+  class View  < ResponderManager
+
+    ## Managing the View Hierarchy
+
+    # superview
+
+    # add_subview
+
+    # subviews
+
+    # set_subviews
+
+
+    ## Drawing
+
+    # display
+
+
+    ## Key-view Loop Management
+
+    # canBecomeKeyView
+
+    # nextKeyView
+
+    #
+
+  end
+
+end
