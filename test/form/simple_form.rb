8505ce3cb8998921b0d1ba5a33157867c5edc373
Matthieu Cormier <mcormier@cs.dal.ca>
2014-12-22 17:12:25 -0400

    Wrap a form in an application.

--- test/form/simple_form.rb
+++ test/form/simple_form.rb
@@ -6,8 +6,7 @@ require_relative '../../lib/ppcurses.rb'
 
 
 
-def display_form
-  form = PPCurses::Form.new(Curses.stdscr)
+  form = PPCurses::Form.new
 
   first_name = PPCurses::InputElement.new('First Name', 10)
   last_name = PPCurses::InputElement.new(' Last Name', 10)
@@ -20,18 +19,9 @@ def display_form
   form.add(age)
   form.add(gender)
 
-  form.handle_input
-
-  { first_name: first_name.value, last_name: last_name.value, age: age.value,
-    gender: gender.current_option, form_submitted: form.submitted?}
-end
 
 
+app = PPCurses::Application.new
+app.content_view = form
 
-
-screen = PPCurses::Screen.new
-form_response = screen.run { display_form }
-
-puts 'Form response data: '
-puts form_response
-
+app.launch
