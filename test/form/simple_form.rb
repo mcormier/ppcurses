d19db9c86f06bb8d39dbce515565ef65c39dce35
Matthieu Cormier <mcormier@cs.dal.ca>
2015-01-02 17:18:11 -0400

    Rewrite example so that the buttons can now terminate the app.

--- test/form/simple_form.rb
+++ test/form/simple_form.rb
@@ -3,7 +3,17 @@
 require 'rubygems'
 require_relative '../../lib/ppcurses.rb'
 
+@app = PPCurses::Application.new
 
+def handle_cancel
+  @app.terminate
+  @pressed_cancel = true
+end
+
+def handle_submit
+  @app.terminate
+  @pressed_submit = true
+end
 
 
 form = PPCurses::Form.new
@@ -13,15 +23,26 @@ last_name = PPCurses::InputElement.new(' Last Name', 10)
 age = PPCurses::InputElement.new_integer_only('      Age', 5)
 
 gender = PPCurses::RadioButtonGroup.new('      Sex', %w(Male Female))
+button_pair = PPCurses::ButtonPair.new('Submit', 'Cancel')
+
+button_pair.button1.action = method(:handle_submit)
+button_pair.button2.action = method(:handle_cancel)
 
 form.add(first_name)
 form.add(last_name)
 form.add(age)
 form.add(gender)
-form.add( PPCurses::ButtonPair.new('Submit', 'Cancel') )
+form.add(button_pair)
 
 
-app = PPCurses::Application.new
-app.content_view = form
 
-app.launch
+@app.content_view = form
+@app.launch
+
+if @pressed_cancel
+  puts 'The user pressed cancel.'
+end
+
+if @pressed_submit
+  puts 'The user pressed submit.'
+end
