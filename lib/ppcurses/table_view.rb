2d5130d07951da1e7831e1d33730a5286b2f31e5
Matthieu Cormier <mcormier@cs.dal.ca>
2015-01-06 17:00:05 -0400

    Whoops.  Should be in the module.

--- lib/ppcurses/table_view.rb
+++ lib/ppcurses/table_view.rb
@@ -1,3 +1,4 @@
+module PPCurses
 # Based loosely on ...
 #
 # https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSTableView_Class/index.html#//apple_ref/occ/cl/NSTableView
@@ -21,5 +22,7 @@ class TableView < View
     PPCurses.implements_protocol( val, %w(number_of_rows_in_table object_value_for ))
     @data_source = val
   end
+
+end
 
 end
