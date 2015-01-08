551108b0906b6011e9851b64ce3afd412ba19cd7
Matthieu Cormier <mcormier@cs.dal.ca>
2015-01-07 21:10:52 -0400

    Use a tableview

--- test/form/menu_opens_form.rb
+++ test/form/menu_opens_form.rb
@@ -3,28 +3,13 @@
 require 'rubygems'
 require_relative '../../lib/ppcurses.rb'
 
+@app = PPCurses::Application.new
+
+@win = PPCurses::Window.new(9,60,0,0)
+
+table_view = PPCurses::TableView.new
 
 
-def display(screen)
-  menu = PPCurses::Menu.new( [ 'About PPCurses', 'Preferences', 'Quit'], nil )
-
-  #screen.add_listener( menu )
-
-  @win = PPCurses::Window.new(9,60,0,0)
-
-  @form = PPCurses::Form.new(@win)
-  media = PPCurses::RadioButtonGroup.new('      Media Type', %w(CD Vinyl MP3) )
-  @form.add(media)
-
-
-  menu.set_global_action(@form)
-
-  menu.show
-  menu.menu_selection
-  menu.close
-end
-
-
-screen = PPCurses::Screen.new
-screen.run { display(screen) }
+@app.content_view = table_view
+@app.launch
 
