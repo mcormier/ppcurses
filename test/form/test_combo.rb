2f95c7f4470fc3ec20f3eb30b7eca411b09b4349
Matthieu Cormier <mcormier@cs.dal.ca>
2015-01-23 20:40:21 -0400

    Revise test to latest interfaces.

--- test/form/test_combo.rb
+++ test/form/test_combo.rb
@@ -4,29 +4,17 @@ require 'rubygems'
 require_relative '../../lib/ppcurses.rb'
 
 
+@app = PPCurses::Application.new
+
+form = PPCurses::Form.new
+
+state_box = PPCurses::ComboBox.new('State',
+                                   ['Maine', 'New York', 'Kansas', 'California'])
+form.add(state_box)
 
 
-def display_form(screen)
-
-  form = PPCurses::Form.new(screen)
-
-  state_box = PPCurses::ComboBox.new('State',
-                                     ['Maine', 'New York', 'Kansas', 'California'])
-
-  form.add(state_box)
-
-  form.handle_input
-
-  { state_selected: state_box.object_value_of_selected_item,
-    form_submitted: form.submitted?}
-end
+@app.content_view = form
+@app.launch
 
 
-
-
-screen = PPCurses::Screen.new
-form_response = screen.run { display_form(screen) }
-
-puts 'Form response data: '
-puts form_response
 
