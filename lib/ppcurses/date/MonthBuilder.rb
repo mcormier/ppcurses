eeb80c8ace409b7f5d7f9edcc1a9859552f8cb4e
Matthieu Cormier <mcormier@cs.dal.ca>
2015-05-09 14:41:41 -0300

    Add temporary mock constructor

--- lib/ppcurses/date/MonthBuilder.rb
+++ lib/ppcurses/date/MonthBuilder.rb
@@ -18,6 +18,25 @@ module PPCurses
     attr_accessor :day_row
     attr_accessor :day_col
  
+    def initialize(day)
+    
+      # TODO - this needs to be dynamic.  Currently hard-coded to may 15th 2015
+      @day = Date.new(2015, 5, 9)
+      
+      @monthStrArray = ["      May 2015      ", 
+                        " S  M Tu  W Th  F  S", 
+                        "                1  2", 
+                        " 3  4  5  6  7  8  9", 
+                        "10 11 12 13 14 15 16", 
+                        "17 18 19 20 21 22 23", 
+                        "24 25 26 27 28 29 30", 
+                        "31                  "]
+       @day_row = 4
+       @day_col = 20
+
+    
+    end
+    
   
   end
 
