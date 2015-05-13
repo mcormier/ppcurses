38db5ee962a9aba71bf88de6d259c850b2fdf4d9
Matthieu Cormier <mcormier@cs.dal.ca>
2015-05-13 17:00:18 -0300

    Add rect and size implementation.

--- lib/ppcurses/geometry.rb
+++ lib/ppcurses/geometry.rb
@@ -1,6 +1,5 @@
 module PPCurses
 
-
   class Point
 
     attr_accessor :x, :y
@@ -16,8 +15,35 @@ module PPCurses
 
   end
 
-  class Rect
+# -------------------------------------------------------------------
 
+  class Size
+    attr_accessor :width, :height
+    
+    def initialize( width, height )
+      @width = width
+      @height = height
+    end
+    
+  end
+
+# -------------------------------------------------------------------
+
+  class Rect
+    attr_accessor :origin, :size
+   
+    def initialize ( origin, size )
+      @origin = origin
+      @size = size
+    end
+   
+    def Rect.makeRect( x, y, w, h )
+      origin = Point.new(x, y)
+      size  = Size.new(w, h)
+      
+      rect = Rect.new( origin, size)
+    end
+   
   end
 
 end
