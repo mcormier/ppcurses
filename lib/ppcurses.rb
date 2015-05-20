d86699b52b8754fb7b339e77e1dc8930389267e1
Matthieu Cormier <mcormier@cs.dal.ca>
2015-05-20 11:10:35 -0300

    Remove getIntegerAction.

--- lib/ppcurses.rb
+++ lib/ppcurses.rb
@@ -50,7 +50,6 @@ module PPCurses
  # Actions -------------------------------------------------------------------------------------------------------------
  require_relative  'ppcurses/actions/ShowMenuAction.rb'
  require_relative  'ppcurses/actions/GetStringAction.rb'
- require_relative  'ppcurses/actions/GetIntegerAction.rb'
  require_relative  'ppcurses/actions/NulAction.rb'
 
  # Forms ---------------------------------------------------------------------------------------------------------------
